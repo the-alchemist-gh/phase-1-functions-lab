07a0fd6716c2d9e79f4a6d408ff75cf7d15b3563
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-04-26 06:33:11 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+  return Math.abs(blocks-42); 
+};
+
+function distanceFromHqInFeet(block){
+  return (Math.abs(block-42)*264);
+}
+
+function distanceTravelledInFeet(dis1,dis2){
+  return (Math.abs(dis2-dis1)*264)
+
+}
+
+function calculatesFarePrice(start, destination){
+  if(distanceTravelledInFeet(start,destination) < 400){
+    return 0;
+  } else if(distanceTravelledInFeet(start,destination) >= 400 && distanceTravelledInFeet(start,destination) <= 2000){
+    return ((distanceTravelledInFeet(start,destination) - 400)*0.02);
+  } else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) < 2500){
+    return 25;
+  } else if(distanceTravelledInFeet(start,destination) > 2500){
+    return "cannot travel that far";
+  }
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,5 @@
 describe('index.js', function() {
-  describe('distanceFromHqInBlocks()', function() {
+  describe ('distanceFromHqInBlocks()', function() {
     it('returns a distance in blocks', function() {
       expect(distanceFromHqInBlocks(43)).to.equal(1);
     });
